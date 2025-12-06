f3995d4caec498feb6250e057d4de68205023e4c
Nesberry LLC <nesberry.apps@gmail.com>
2025-12-06 18:41:22 +0000

    The app isn't starting. Please investigate what could be wrong based on

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
-import type {NextConfig} from 'next';
-
-const nextConfig: NextConfig = {
+/**
+ * @type {import('next').NextConfig}
+ */
+const nextConfig = {
   output: 'standalone',
   images: {
     remotePatterns: [
@@ -17,5 +18,3 @@ const nextConfig: NextConfig = {
 };
 
 export default nextConfig;
-
-    
