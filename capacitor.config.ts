e76583e1b7fbe83afdacf71025d12b5e933c975c
Nesberry LLC <nesberry.apps@gmail.com>
2025-12-24 02:44:21 +0000

    There still seems to be an error in the package.json file you updated?

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -12,16 +12,9 @@ const config: CapacitorConfig = {
   plugins: {
     // Firebase Analytics and other plugins are configured here now
   },
-  // This is the key change for the iOS build
+  // Temporarily removed AdMob and Purchases for a clean iOS build
   ios: {
-    packageClassList: [
-      'AdMobPlugin',
-      'FirebaseAnalyticsPlugin',
-      'FilesystemPlugin',
-      'LocalNotificationsPlugin',
-      'PushNotificationsPlugin',
-      'PurchasesPlugin' // Correct name for the latest SDK
-    ]
+    packageClassList: []
   }
 };
 
