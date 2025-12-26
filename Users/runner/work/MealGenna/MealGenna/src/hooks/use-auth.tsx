dbed696ad0fcd8949047be5327bfdec13a2d1304
Nesberry LLC <nesberry.apps@gmail.com>
2025-12-26 16:26:13 +0000

    that didnt work

--- Users/runner/work/MealGenna/MealGenna/src/hooks/use-auth.tsx
+++ Users/runner/work/MealGenna/MealGenna/src/hooks/use-auth.tsx
@@ -1,3 +1,2 @@
 // This file is intentionally left blank.
 // All authentication-related code has been removed.
-    
--- Users/runner/work/MealGenna/MealGenna/src/lib/firebase-client.ts
+++ Users/runner/work/MealGenna/MealGenna/src/lib/firebase-client.ts
@@ -1,3 +1,2 @@
 // This file is intentionally left blank.
 // All Firebase client-side code has been removed.
-    
