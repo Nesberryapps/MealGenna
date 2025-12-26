b135ec087fbff60768a8552cdba130e619e7de1c
Nesberry LLC <nesberry.apps@gmail.com>
2025-12-26 01:32:18 +0000

    SInce your incompetency has caused me so much disapointment I did some r

--- Users/runner/work/MealGenna/MealGenna/src/app/api/account/recover/route.ts
+++ Users/runner/work/MealGenna/MealGenna/src/app/api/account/recover/route.ts
@@ -1,6 +1,6 @@
 
 import { NextResponse } from 'next/server';
-import { admin } from '../../../lib/firebase-admin';
+import { admin } from '../../../../lib/firebase-admin';
 
 export async function POST(req: Request) {
   try {
