e92f8a82e1894f3263d8b4e798b11de83dfda0f7
Nesberry LLC <nesberry.apps@gmail.com>
2025-12-26 01:18:29 +0000

    Try fixing this error: `Build Error: Failed to compile. Error source: ./

--- src/app/api/account/verify/route.ts
+++ src/app/api/account/verify/route.ts
@@ -1,6 +1,6 @@
 
 import { NextResponse } from 'next/server';
-import { admin } from '../../../lib/firebase-admin';
+import { admin } from '../../../../lib/firebase-admin';
 import { getFirestore } from 'firebase-admin/firestore';
 
 export async function POST(req: Request) {
