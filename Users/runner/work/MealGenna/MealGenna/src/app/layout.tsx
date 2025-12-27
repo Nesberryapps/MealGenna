bb2da9b70013d2a98e3590a6e025faf7f87ad80c
Nesberry LLC <nesberry.apps@gmail.com>
2025-12-27 03:46:01 +0000

    here we go again.... the build failed with the same error message:
    fail

--- Users/runner/work/MealGenna/MealGenna/src/app/layout.tsx
+++ Users/runner/work/MealGenna/MealGenna/src/app/layout.tsx
@@ -1,7 +1,7 @@
 
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import "./globals.css";
+import "../globals.css";
 import { Toaster } from "@/components/ui/toaster";
 import Link from "next/link";
 import { ChefHat, User } from "lucide-react";
