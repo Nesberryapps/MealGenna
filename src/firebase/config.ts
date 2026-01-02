5abd98987a733bff2b40bb80716f2dc29b16e93b
Nesberry LLC <nesberry.apps@gmail.com>
2026-01-02 22:26:00 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-8280698092-68a45",
+  "appId": "1:266513828882:web:6995b55eeb35540c351ccb",
+  "apiKey": "AIzaSyD-Mo5KiOidqqIPBLaADk62tQ6ZtgHkhvY",
+  "authDomain": "studio-8280698092-68a45.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "266513828882"
+};
