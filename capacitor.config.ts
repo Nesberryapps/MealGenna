3501bd9c1a9cc70e98dd16ef4f84356f7aae5e03
Nesberry LLC <nesberry.apps@gmail.com>
2026-01-03 18:24:01 +0000

    the appId should actually be: com.nesberry.mealgenna.pro This already ex

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.mealgenna.app',
+  appId: 'com.nesberry.mealgenna.pro',
   appName: 'MealGenna',
   webDir: 'out',
   server: {
