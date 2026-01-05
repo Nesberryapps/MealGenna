5dc0f1d1d3cc83bd88748d737a4589fc7665a857
Nesberry LLC <nesberry.apps@gmail.com>
2026-01-05 08:18:37 +0000

    There seems to be another failed build for both and the error message se

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,6 @@ import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
-  output: 'export',
   typescript: {
     ignoreBuildErrors: true,
   },
@@ -38,16 +37,6 @@ const nextConfig: NextConfig = {
         pathname: '/**',
       },
     ],
-  },
-  publicRuntimeConfig: {
-    firebase: {
-      apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-      authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-      projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-      storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-      messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-      appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-    },
   },
 };
 
