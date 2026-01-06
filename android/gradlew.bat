1973dd7dc7910a3065f80ee694bbd859163b6aaf
Nesberry LLC <nesberry.apps@gmail.com>
2026-01-06 05:36:59 +0000

    so everthing was a succes until I ran the npx cap sync command, as shown

--- android/gradlew.bat
+++ android/gradlew.bat
@@ -1,43 +1,47 @@
-::
-:: Copyright 2015 the original author or authors.
-::
-:: Licensed under the Apache License, Version 2.0 (the "License");
-:: you may not use this file except in compliance with the License.
-:: You may obtain a copy of the License at
-::
-::      https://www.apache.org/licenses/LICENSE-2.0
-::
-:: Unless required by applicable law or agreed to in writing, software
-:: distributed under the License is distributed on an "AS IS" BASIS,
-:: WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-:: See the License for the specific language governing permissions and
-:: limitations under the License.
-::
+@rem
+@rem Copyright 2015 the original author or authors.
+@rem
+@rem Licensed under the Apache License, Version 2.0 (the "License");
+@rem you may not use this file except in compliance with the License.
+@rem You may obtain a copy of the License at
+@rem
+@rem      https://www.apache.org/licenses/LICENSE-2.0
+@rem
+@rem Unless required by applicable law or agreed to in writing, software
+@rem distributed under the License is distributed on an "AS IS" BASIS,
+@rem WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+@rem See the License for the specific language governing permissions and
+@rem limitations under the License.
+@rem
 
-@if "%DEBUG%" == "" @echo off
-:: ##########################################################################
-::
-::  Gradle start up script for Windows
-::
-:: ##########################################################################
+@if "%DEBUG%"=="" @echo off
+@rem ##########################################################################
+@rem
+@rem  Gradle startup script for Windows
+@rem
+@rem ##########################################################################
 
-:: Set local scope for the variables with windows NT shell
+@rem Set local scope for the variables with windows NT shell
 if "%OS%"=="Windows_NT" setlocal
 
-:: Add default JVM options here. You can also use JAVA_OPTS and GRADLE_OPTS to pass JVM options to this script.
-set DEFAULT_JVM_OPTS=-Xmx64m -Xms64m
-
 set DIRNAME=%~dp0
-if "%DIRNAME%" == "" set DIRNAME=.
+if "%DIRNAME%"=="" set DIRNAME=.
+@rem This is normally unused
 set APP_BASE_NAME=%~n0
 set APP_HOME=%DIRNAME%
 
-:: Find java.exe
+@rem Resolve any "." and ".." in APP_HOME to make it shorter.
+for %%i in ("%APP_HOME%") do set APP_HOME=%%~fi
+
+@rem Add default JVM options here. You can also use JAVA_OPTS and GRADLE_OPTS to pass JVM options to this script.
+set DEFAULT_JVM_OPTS="-Xmx64m" "-Xms64m"
+
+@rem Find java.exe
 if defined JAVA_HOME goto findJavaFromJavaHome
 
 set JAVA_EXE=java.exe
 %JAVA_EXE% -version >NUL 2>&1
-if "%ERRORLEVEL%" == "0" goto execute
+if %ERRORLEVEL% equ 0 goto execute
 
 echo.
 echo ERROR: JAVA_HOME is not set and no 'java' command could be found in your PATH.
@@ -49,7 +53,8 @@ goto fail
 
 :findJavaFromJavaHome
 set JAVA_HOME=%JAVA_HOME:"=%
-set JAVA_EXE=%JAVA_HOME%/b
+set JAVA_EXE=%JAVA_HOME%/bin/java.exe
+
 if exist "%JAVA_EXE%" goto execute
 
 echo.
@@ -61,44 +66,25 @@ echo location of your Java installation.
 goto fail
 
 :execute
-:: Setup the command line
+@rem Setup the command line
 
-:: Verify that the user has this script in the gradlew directory.
-if not exist "%APP_HOME%\gradlew" (
-    :: Ok, we will also allow this script to be in the parent directory.
-    if not exist "%APP_HOME%\..\gradlew" (
-        echo The gradlew script must be run from the gradlew directory or its parent directory.
-        goto fail
-    )
-)
+set CLASSPATH=%APP_HOME%\gradle\wrapper\gradle-wrapper.jar
 
-:: Set-up parameters for Gradle wrapper.
-set GRADLE_WRAPPER_JAR="%APP_HOME%\gradle\wrapper\gradle-wrapper.jar"
-set GRADLE_WRAPPER_PROPERTIES="%APP_HOME%\gradle\wrapper\gradle-wrapper.properties"
 
-:: Get the application name from the gradle-wrapper.properties file
-set GRADLE_APP_NAME_BYTES_STRING=
-for /f "tokens=1,2 delims==" %%a in ('findstr "gradle.app.name" "%GRADLE_WRAPPER_PROPERTIES%"') do (
-    if "%%a"=="gradle.app.name" (
-        set GRADLE_APP_NAME_STRING=%%b
-        set GRADLE_APP_NAME_BYTES_STRING=-Dgradle.app.name=%%b
-    )
-)
-
-:: Execute Gradle
-%JAVA_EXE% %DEFAULT_JVM_OPTS% %JAVA_OPTS% %GRADLE_OPTS% %GRADLE_APP_NAME_BYTES_STRING% -jar %GRADLE_WRAPPER_JAR% %*
+@rem Execute Gradle
+"%JAVA_EXE%" %DEFAULT_JVM_OPTS% %JAVA_OPTS% %GRADLE_OPTS% "-Dorg.gradle.appname=%APP_BASE_NAME%" -classpath "%CLASSPATH%" org.gradle.wrapper.GradleWrapperMain %*
 
 :end
-:: End local scope for the variables with windows NT shell
-if "%ERRORLEVEL%"=="0" goto mainEnd
+@rem End local scope for the variables with windows NT shell
+if %ERRORLEVEL% equ 0 goto mainEnd
 
 :fail
 rem Set variable GRADLE_EXIT_CONSOLE if you need the _script_ return code instead of
-rem the _cmd.exe /c_ return code.
-if not "" == "%GRADLE_EXIT_CONSOLE%" (
-  exit 1
-)
-exit /b 1
+rem the _cmd.exe /c_ return code!
+set EXIT_CODE=%ERRORLEVEL%
+if %EXIT_CODE% equ 0 set EXIT_CODE=1
+if not ""=="%GRADLE_EXIT_CONSOLE%" exit %EXIT_CODE%
+exit /b %EXIT_CODE%
 
 :mainEnd
 if "%OS%"=="Windows_NT" endlocal
--- ios/App/build-and-export.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -ex
-
-# Build and archive the app
-xcodebuild -workspace App.xcworkspace -scheme App -configuration Release -archivePath "$RUNNER_TEMP/App.xcarchive" archive
-
-# Export the archive to an IPA
-xcodebuild -exportArchive -archivePath "$RUNNER_TEMP/App.xcarchive" -exportPath "$RUNNER_TEMP/build" -exportOptionsPlist exportOptions.plist
