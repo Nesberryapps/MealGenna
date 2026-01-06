712a95ea195b4d8c43e7c00bfecc35a93ecb27c4
Nesberry LLC <nesberry.apps@gmail.com>
2026-01-06 05:24:43 +0000

    lets get this app back on track to be published on the app stores  I not

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -4,7 +4,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.nesberry.mealgenna.pro',
   appName: 'MealGenna',
-  webDir: 'public',
+  webDir: 'out',
   server: {
     androidScheme: 'https',
   },
