333bd5acb8fd86d6ab2a926f5b4395d25b0c7c9f
Nesberry LLC <nesberry.apps@gmail.com>
2026-01-06 21:14:03 +0000

    fix server issue

--- next.config.ts
+++ next.config.ts
@@ -3,8 +3,7 @@ import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
-  output: 'export',
-  trailingSlash: true,
+  trailingSlash: false,
   images: {
     unoptimized: true,
     remotePatterns: [
--- src/app/api/genkit/[...slug]/route.ts
+++
@@ -1,8 +0,0 @@
-/**
- * This file is the single point of entry for all Genkit flow requests.
- */
-import {nextHandler} from '@genkit-ai/next';
-import ai from '@/ai/genkit';
-
-// Expose the Genkit API endpoint.
-export const POST = nextHandler({ai});
