987c663a2a154a2afdc9981794d2bef7754e5db8
Nesberry LLC <nesberry.apps@gmail.com>
2026-01-07 03:10:39 +0000

    update package.json

--- create-keystore-properties.sh
+++ create-keystore-properties.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
+# Get absolute path to the keystore file which we placed in android/app/release.keystore
+# The script runs from root, so PWD/android/app/release.keystore is the path.
+KEYSTORE_PATH="$PWD/android/app/release.keystore"
+
 echo "storePassword=$ANDROID_KEYSTORE_PASSWORD" > android/keystore.properties
 echo "keyPassword=$ANDROID_KEY_PASSWORD" >> android/keystore.properties
 echo "keyAlias=$ANDROID_KEY_ALIAS" >> android/keystore.properties
-echo "storeFile=release.keystore" >> android/keystore.properties
+echo "storeFile=$KEYSTORE_PATH" >> android/keystore.properties
