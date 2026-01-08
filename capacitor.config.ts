d64da3400c07d67c8c0441454640238d3791eeec
Nesberry LLC <nesberry.apps@gmail.com>
2026-01-08 06:49:08 +0000

    fisx the apps features

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -5,6 +5,10 @@ const config: CapacitorConfig = {
   appId: 'com.nesberry.mealgenna.pro',
   appName: 'MealGenna',
   webDir: 'out',
+  server: {
+    url: 'http://localhost:3000',
+    cleartext: true
+  }
 };
 
 export default config;
